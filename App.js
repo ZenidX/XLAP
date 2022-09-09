6babf7177351ef825edca28a61962b634aea03a1
ZenidX <zenid77@gmail.com>
2022-09-09 16:39:50 +0200

    cambiamos de sentido, seguimos subiendo de 2 a 1

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          DOOMSLAYER
+          GIT
         </p>
         <a
           className="App-link"
